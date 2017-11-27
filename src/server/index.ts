124555b0320bc0f3597fc0c79ccf614fdb28a4ba
Kyle Kemp <kyle@seiyria.com>
2017-11-27 08:23:36 -0600

    process forking

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,27 +7,39 @@ import { Server } from 'rivercut';
 import { LobbyRoom } from './lobby.room';
 import { GameRoom } from './multincgame.room';
 
-const server = new Server({
-  namespace: 'multincrole',
-  resetStatesOnReboot: ['Game'],
-  serializeByRoomId: true
-});
+import * as cluster from 'cluster';
+import * as os from 'os';
 
-console.log('Initializing...');
-server.init(process.env.DEEPSTREAM_URL, {});
-
-console.log('Registering...');
-server.registerRoom('Lobby', LobbyRoom, { singleInstance: true, serializeByRoomId: false });
-server.registerRoom('Game', GameRoom, {});
-
-console.log('Logging in...');
-server.login({ token: process.env.DEEPSTREAM_TOKEN })
-  .then(() => {
-    console.log(`Logged In - Multinc Role - Server - ${server.uid}`);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+const numForks = process.env.NODE_ENV === 'dev' ? 1 : os.cpus().length;
 
 process.on('uncaughtException', (err) => console.error(err));
 process.on('unhandledRejection', (err) => console.error(err));
+
+if(cluster.isMaster) {
+  for(let i = 0; i < numForks; i++) {
+    cluster.fork();
+  }
+
+} else {
+  const server = new Server({
+    namespace: 'multincrole',
+    resetStatesOnReboot: ['Game'],
+    serializeByRoomId: true
+  });
+
+  console.log(`${process.pid} | Initializing...`);
+  server.init(process.env.DEEPSTREAM_URL, {});
+
+  console.log(`${process.pid} | Registering...`);
+  server.registerRoom('Lobby', LobbyRoom, { singleInstance: true, serializeByRoomId: false });
+  server.registerRoom('Game', GameRoom, {});
+
+  console.log(`${process.pid} | Logging in...`);
+  server.login({ token: process.env.DEEPSTREAM_TOKEN })
+    .then(() => {
+      console.log(`${process.pid} | Logged In - Multinc Role - Server - ${server.uid}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
